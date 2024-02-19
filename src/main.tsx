9c2472a802ae288a38390423793813e4b5a57dd2
Dima <dimadeg2202@gmail.com>
2024-02-19 13:23:02 +0500

    Added queryProvider for tanstack and Provider for redux

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import Router from 'routers/Router'
+
+import './assets/styles/index.scss'
+import store from 'store/store'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<Router />
+			</Provider>
+		</QueryClientProvider>
+	</React.StrictMode>
+)
