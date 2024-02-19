3ce6a1914f6d58f4855917e2ba8ef6f1a4dc9c24
Dima <dimadeg2202@gmail.com>
2024-02-19 13:38:25 +0500

    Redux store

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import playerReducer from './playerSlice'
+
+
+const store = configureStore({
+  reducer: {
+    player: playerReducer,
+  }
+})
+
+export default store
+
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppDispatch = typeof store.dispatch
