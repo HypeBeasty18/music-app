91b970f2f867150e2610d35a73360e6269fde3e1
Dima <dimadeg2202@gmail.com>
2024-02-20 18:25:12 +0500

    Nothing important

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,11 @@
-import axios from "axios"
+import axios from 'axios'
 
-
-const SHAZAM_CORE_API_URL =  import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
+const SHAZAM_CORE_API_URL = import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY
 
 export const $axiosShazam = axios.create({
-  baseURL: SHAZAM_CORE_API_URL,
-  headers: {
-    'X-RapidAPI-Key': '7445b69481msh94fb4b6d527963dp12062cjsn8ac318a01544',
-    'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-  }
+	baseURL: SHAZAM_CORE_API_URL,
+	headers: {
+		'X-RapidAPI-Key': 'd5192e61demshcb467a7e72408abp1b0cf0jsned15d31886ef',
+		'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+	}
 })
