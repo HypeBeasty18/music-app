54edbeea6adb3f8ce634096b3588841d465bba17
Dima <dimadeg2202@gmail.com>
2024-02-20 18:29:40 +0500

    Added exceptions

--- src/components/ui/SongCard.tsx
+++ src/components/ui/SongCard.tsx
@@ -28,27 +28,43 @@ const SongCard: FC<Props> = ({ song, index }) => {
 
 	return (
 		<div className=' flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer select-none	'>
-			<div className='relative h-56 w-full mb-5 group'>
-				<div
-					className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}
-				>
-					<PlayPause
-						handlePlay={handlePlayClick}
-						handlePause={handlePauseClick}
-						activeSong={activeSong}
-						song={song}
-						isPlaying={isPlaying}
+			{song.images ? (
+				<div className='relative h-56 w-full mb-5 group'>
+					<div
+						className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}
+					>
+						<PlayPause
+							handlePlay={handlePlayClick}
+							handlePause={handlePauseClick}
+							activeSong={activeSong}
+							song={song}
+							isPlaying={isPlaying}
+						/>
+					</div>
+					<img
+						src={song.images.coverart}
+						className='w-full h-full rounded-lg'
 					/>
 				</div>
-				<img src={song.images.coverart} className='w-full h-full rounded-lg' />
-			</div>
+			) : (
+				<p className='relative h-56 w-full mb-5 flex justify-center items-center text-2xl text-white'>
+					Not image
+				</p>
+			)}
+
 			<div className='text-white'>
 				<p className='font-bold text-base mb-2 truncate'>
 					<Link to={`/songs/${song?.key}`}>{song.title}</Link>
 				</p>
-				<p className='font-normal text-sm text-darkGrey truncate'>
-					<Link to={`/artists/${song.artists[0].adamid}`}>{song.subtitle}</Link>
-				</p>
+				{song.artists ? (
+					<p className='font-normal text-sm text-darkGrey truncate'>
+						<Link to={`/artists/${song.artists[0].adamid}`}>
+							{song.subtitle}
+						</Link>
+					</p>
+				) : (
+					<span>Not Found</span>
+				)}
 			</div>
 		</div>
 	)
