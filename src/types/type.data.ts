34cc9668c5ab9f995f1f35b86dab27c3c241c1ef
Dima <dimadeg2202@gmail.com>
2024-02-20 18:34:02 +0500

    Added type for found songs and artist

--- src/types/type.data.ts
+++ src/types/type.data.ts
@@ -1,25 +1,37 @@
 export type Song = {
-		artists: [
+	artists: [
+		{
+			adamid: string
+		}
+	]
+	title: string
+	subtitle: string
+	images?: {
+		coverart: string
+	}
+	key: string
+	hub: {
+		actions: [
 			{
-				adamid: string
+				name: string
+			},
+			{
+				name: string
+				type: string
+				uri: string
 			}
 		]
-		title: string
-		subtitle: string
-		images: {
-			coverart: string
-		}
-		key: string
-		hub: {
-			actions: [
-				{
-					name: string
-				},
-				{
-					name: string
-					type: string
-					uri: string
-				}
-			]
-		}
 	}
+}
+
+export type FindedSong = {
+	track: Song
+}
+
+export type Artist = {
+	artist: {
+		adamid: string
+		avatar: string
+		name: string
+	}
+}
