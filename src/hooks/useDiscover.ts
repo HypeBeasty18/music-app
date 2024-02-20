b5e37e951b1b6778de448d8090d25aca7332f5f4
Dima <dimadeg2202@gmail.com>
2024-02-20 18:30:34 +0500

    Added infinite scrolling function

--- src/hooks/useDiscover.ts
+++ src/hooks/useDiscover.ts
@@ -1,21 +1,39 @@
-import { useQuery } from '@tanstack/react-query'
-import { useEffect, useMemo } from 'react'
+import { useMutation } from '@tanstack/react-query'
 import { GetSpotify } from 'services/GetSpotify.service'
 
-import { useActions } from './useActions'
+import { useActions, useAppSelector } from './useActions'
 
-export const useDiscover = (start: number) => {
+interface Props {
+	value: number
+	setValue: React.Dispatch<React.SetStateAction<number>>
+}
+
+export const useDiscover = ({ value, setValue }: Props) => {
 	const actions = useActions()
 
-	const { data, isLoading, error } = useQuery({
-		queryKey: ['top20'],
-		queryFn: () => GetSpotify.GetSongs(start)
-	})
-	useEffect(() => {
-		if (data?.tracks) {
-			actions.setCurrentSongs(data.tracks)
-		}
-	}, [isLoading])
+	const currentSongs = useAppSelector(state => state.player.currentSongs)
 
-	return useMemo(() => ({ isLoading, error }), [isLoading, error])
+	const { mutate, isPending, error } = useMutation({
+		mutationKey: [`getSongs`],
+		mutationFn: (value: number) => GetSpotify.GetSongs(value),
+		onSuccess(data) {
+			if (value === 0) {
+				actions.setCurrentSongs(data.tracks)
+				setValue(value + 20)
+			} else {
+				actions.setCurrentSongs(currentSongs.concat(data.tracks))
+				setValue(value + 20)
+			}
+		}
+	})
+
+	return { isPending, error, mutate }
 }
+
+// 479756766
+
+// 2715720
+
+// 271256
+// 1031397873
+// 183313439
