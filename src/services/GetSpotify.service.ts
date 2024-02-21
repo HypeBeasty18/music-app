f1b4695b4d6d1590f00fce851a31fa85fc577d1a
Dima <dimadeg2202@gmail.com>
2024-02-21 16:41:59 +0500

    Added axios requests to current geo and songs in geo

--- src/services/GetSpotify.service.ts
+++ src/services/GetSpotify.service.ts
@@ -1,4 +1,5 @@
-import { $axiosShazam } from 'api/api'
+import { $axiosShazam, $axiosShazamApi } from 'api/api'
+import axios from 'axios'
 
 export const GetSpotify = {
 	async GetSongs(start: number) {
@@ -21,6 +22,31 @@ export const GetSpotify = {
 					locale: 'en-US',
 					offset: '0',
 					limit: '5'
+				}
+			})
+			return res.data
+		} catch (error) {
+			throw new Error()
+		}
+	},
+	async CurrentLocation() {
+		try {
+			const res = await axios.get(
+				`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_GEOLOCATION_API_KEY}`
+			)
+				console.log(res.data.location.country);
+				
+			return res.data.location.country
+		} catch (error) {
+			throw new Error()
+		}
+	},
+	async GetAroundYouSongs(country: string) {
+		try {
+			const res = await $axiosShazamApi.get('/top_tracks_country', {
+				params: {
+					country_code: country,
+					limit: '40'
 				}
 			})
 			return res.data
