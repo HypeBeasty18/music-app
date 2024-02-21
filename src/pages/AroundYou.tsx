21a02216b715bf1eeb3015ad79d94e0b121747d2
Dima <dimadeg2202@gmail.com>
2024-02-21 16:41:00 +0500

    Top songs in user's country

--- src/pages/AroundYou.tsx
+++ src/pages/AroundYou.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import Loader from 'components/loader/Loader'
+import SongCard from 'components/ui/SongCard'
+import { useAppSelector } from 'hooks/useActions'
+import { useAroundYou } from 'hooks/useAroundYou'
+import { FC } from 'react'
 
-type Props = {}
+const AroundYou: FC = () => {
+	const { data, error, mutate, isPending } = useAroundYou()
 
-const AroundYou = (props: Props) => {
-  return (
-    <div>AroundYou</div>
-  )
+	const currentSongs = useAppSelector(state => state.player.currentSongs)
+
+	return (
+		<div>
+			<h2 className='title'>
+				Around you {data ? <span className='text-grey'>[ {data} ]</span> : null}
+			</h2>
+			{error ? (
+				<div>{error.message}</div>
+			) : isPending ? (
+				<div className='flex justify-center items-center w-full'>
+					<Loader />
+				</div>
+			) : (
+				<div className='flex flex-wrap  gap-8'>
+					{currentSongs.map((song, index) => (
+						<SongCard
+							key={index}
+							song={song.track ? song.track : song}
+							index={index}
+						/>
+					))}
+				</div>
+			)}
+		</div>
+	)
 }
 
 export default AroundYou
