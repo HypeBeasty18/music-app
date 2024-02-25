a2c18921a81e8310aa44c914fc45c2e1694de92b
Dima <93480321+JustDimaOMG@users.noreply.github.com>
2024-02-25 20:32:41 +0500

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,7 @@ const SHAZAM_RAPID_API_KEY = import.meta.env.VITE_SHAZAM_RAPID_API_KEY
 export const $axiosShazam = axios.create({
 	baseURL: SHAZAM_RAPID_API_KEY,
 	headers: {
-		'X-RapidAPI-Key': 'd5192e61demshcb467a7e72408abp1b0cf0jsned15d31886ef',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
 	}
 })
@@ -15,7 +15,7 @@ const SHAZAM_API_RAPID_API_KEY = import.meta.env.VITE_SHAZAM_API_RAPID_API_KEY
 export const $axiosShazamApi = axios.create({
 	baseURL: SHAZAM_API_RAPID_API_KEY,
 	headers: {
-		'X-RapidAPI-Key': 'd5192e61demshcb467a7e72408abp1b0cf0jsned15d31886ef',
+		'X-RapidAPI-Key': '',
 		'X-RapidAPI-Host': 'shazam-api6.p.rapidapi.com'
 	}
 })
